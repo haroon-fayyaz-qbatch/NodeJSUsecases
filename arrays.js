592bf410c0165b8a000249dc73ed93120d178b3e
haroon-fayyaz-qbatch <haroon.fayyaz@qbatch.com>
2021-07-30 14:57:00 +0500

    array of objects using reduce

--- arrays.js
+++ arrays.js
@@ -1,35 +1,63 @@
-let arr = [43, 232, 343, 54];
-console.log("Adding the items: ");
-let sum = 0;
-arr.forEach((item) => {
-  console.log(item + " ");
-  sum += item;
-});
+// let arr = [43, 232, 343, 54];
+// console.log("Adding the items: ");
+// let sum = 0;
+// arr.forEach((item) => {
+//   console.log(item + " ");
+//   sum += item;
+// });
 
-console.log(`\nThe sum is: ${sum}`);
+// console.log(`\nThe sum is: ${sum}`);
 
-// console.log(arr);
+// // console.log(arr);
 
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
 
-const sub = arr.reduce((acc, item) => {
-  console.log(`${acc} : ${item}`);
-  return acc - item;
-});
+// const sub = arr.reduce((acc, item) => {
+//   console.log(`${acc} : ${item}`);
+//   return acc - item;
+// });
 
-console.log(sub);
+// console.log(sub);
 
-const filter = arr.filter((item) => {
-  return item > 300;
-});
+// const filter = arr.filter((item) => {
+//   return item > 300;
+// });
 
-console.log(filter);
-const find = arr.find((item) => {
-  return item > 23;
-});
+// console.log(filter);
+// const find = arr.find((item) => {
+//   return item > 23;
+// });
 
-console.log(find);
+// console.log(find);
 
-arr.findIndex();
+var wizards = [
+  {
+    name: "Harry Potter",
+    house: "Gryfindor",
+  },
+  {
+    name: "Cedric Diggory",
+    house: "Hufflepuff",
+  },
+  {
+    name: "Tonks",
+    house: "Hufflepuff",
+  },
+  {
+    name: "Ronald Weasley",
+    house: "Gryfindor",
+  },
+  {
+    name: "Hermione Granger",
+    house: "Gryfindor",
+  },
+];
+
+let hufflepuff = wizards.reduce((newArr, wizard) => {
+  newArr.push(wizard.name.toLowerCase());
+  return newArr;
+}, []);
+
+console.log(hufflepuff);
